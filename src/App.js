c09e1063a5422c65d19b46d0226291515c6c31c7
Chris Davies <c.m.davies10@gmail.com>
2019-11-23 07:49:52 -0500

    images added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Header from "./components/Header";
+// import Section from "./components/Section";
+import Navbar from "./components/Navbar";
+import Container from "react-bootstrap/Container";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar />
+			<Container>
+				<h1>TEST</h1>
+				<h2>TEST</h2>
+				<h3>TEST</h3>
+				<h4>TEST</h4>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../styles/Navbar.css";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
+
+function NavbarComponent() {
+	return (
+		<div>
+			<Container>
+				<Navbar>
+					<a className="navbar-brand" href="#">
+						GoM
+					</a>
+					<a className="nav-item nav-link" href="/">
+						Link
+					</a>
+					<a className="nav-item nav-link" href="/">
+						Link
+					</a>
+					<a className="nav-item nav-link" href="/">
+						Link
+					</a>
+				</Navbar>
+			</Container>
+		</div>
+	);
+}
+
+export default NavbarComponent;
