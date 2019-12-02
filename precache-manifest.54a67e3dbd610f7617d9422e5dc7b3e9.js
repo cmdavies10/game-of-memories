4f37749fa861326183e5fd15ec0b891d1371d366
Chris Davies <c.m.davies10@gmail.com>
2019-12-02 15:22:05 -0500

    Updates

---
+++ precache-manifest.54a67e3dbd610f7617d9422e5dc7b3e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d623ea8a4fa289928c41126d6b06fd",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "c66e34127a855728a5bc",
+    "url": "/memory-game/static/css/main.b0f67cba.chunk.css"
+  },
+  {
+    "revision": "bdba25dfaf0a9234e62d",
+    "url": "/memory-game/static/js/2.ed1148a7.chunk.js"
+  },
+  {
+    "revision": "c66e34127a855728a5bc",
+    "url": "/memory-game/static/js/main.7c4cb193.chunk.js"
+  },
+  {
+    "revision": "3c6c1d3098f1f9b94d8d",
+    "url": "/memory-game/static/js/runtime-main.53e70fbf.js"
+  }
+]);
